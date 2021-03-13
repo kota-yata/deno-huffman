3c9d42d6d72efed6eb455129c1df6f0afc84ccd0
kota-yata <kota@yatagai.com>
2021-03-13 11:00:46 +0900

    implement encoding

---
+++ src/encode/result.ts
@@ -0,0 +1,21 @@
+import { treeArray } from '../../global.d.ts';
+
+export const generateBitsArray = (tree: treeArray, resultStringArray: string[], resultBitArray: string[] ,bit: string = ''): void => {
+  if(tree[0] !== null) {
+    resultStringArray.push(tree[0]);
+    resultBitArray.push(bit);
+    return;
+  }
+  if(!tree[2]) throw Error('You assign wrong array to generateBits() as argument');
+  generateBitsArray(tree[2][0], resultStringArray, resultBitArray, bit + '0');
+  generateBitsArray(tree[2][1], resultStringArray, resultBitArray, bit + '1');
+}
+
+export const generateResultBits = (plane: string, stringArray: string[], bitArray: string[]): string => {
+  let resultString: string = '';
+  for(let i: number = 0; i < plane.length; i++) {
+    const index: number = stringArray.indexOf(plane[i]);
+    resultString += bitArray[index];
+  }
+  return resultString;
+}
